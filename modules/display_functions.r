4ea9b0af9803aec3af674f32d648c5e143551ab7
benjaminhlina <benjamin.hlina@gmail.com>
2025-12-03 13:56:04 -0500

    Add in display_scatter_plot

--- modules/display_functions.r
+++ modules/display_functions.r
@@ -103,5 +103,114 @@ display_hist <- function(data,
 
 }
 
+# ----- display scatter plot -----
+display_scatter_plot <- function(data,
+                                 input_source,
+                                 output,
+                                 output_id = "scatter_plot") {
+  # ----- display scatter plot -----
+  output[[output_id]] <- renderPlot({
+    #     # Get raw data (not summarized)
+
+    df <- data()
+    # get the x var
+
+    x_var_raw <- input_source$x_choices()
+
+
+    # get the y var
+    y_var_raw <- input_source$y_choices()
+    # get the basic grouping
+    scatter_grouping_vars <- input_source$grouping_vars()
+
+    # use generic function to filter and grab the correct for length only
+    fix_x <- fix_var_generic(
+      df = df,
+      var_raw = x_var_raw,
+      get_nice_name = get_nice_name
+    )
+
+    # get the returned objects which are returned in a list
+    df <- fix_x$df
+    x_var <- fix_x$var
+    x_label <- fix_x$var_label
+
+    # now do the same for y
+    fix_y <- fix_var_generic(
+      df = df,
+      var_raw = y_var_raw,
+      get_nice_name = get_nice_name
+    )
+
+    y_var <- fix_y$var
+    y_label <- fix_y$var_label
+    # filter df by x and y vars
+    df <- df |>
+      filter(!is.na(.data[[x_var]]), !is.na(.data[[y_var]]))
+
+    p <- ggplot(df, aes(
+      x = !!sym(x_var),
+      y = !!sym(y_var))) +
+      theme_bw(base_size = 15) +
+      theme(
+        panel.grid = element_blank(),
+        plot.title = element_markdown(hjust = 0.5),
+        axis.title = element_markdown(),
+        legend.title = element_markdown(),
+        legend.text = element_markdown()
+      ) +
+      labs(
+        x = x_label,
+        y = y_label,
+        title = paste("Scatter Plot of", y_label, "vs", x_label)
+      )
+
+    if (scatter_grouping_vars != "None") {
+      p <- p +
+        geom_point(
+          aes(fill = !!sym(scatter_grouping_vars)),
+          alpha = 0.7,
+          size = 5,
+          shape = 21
+        ) +
+        scale_fill_viridis_d(name = scatter_grouping_vars,
+                             option = "B",
+                             begin = 0.1,
+                             end = 0.9,
+                             alpha = 0.5
+        )
+    } else {
+      p <- p + geom_point(
+        alpha = 0.7,
+        size = 3,
+        shape = 21
+      )
+    }
+    return(p)
+  })
+}
+
+
+# ---- display summary_table -----
+
+display_table <- function(data, output, output_id = "summary_table_output") {
+  output[[output_id]] <- renderDT({
+    req(data())
+    df <- data()
+    # if there is nothing in df print no data available
+    if (is.null(df) || nrow(df) == 0) {
+      return(datatable(data.frame(Message = "No data available"),
+                       escape = FALSE))
+    }
+
+    datatable(df,
+              options = list(pageLength = 10,
+                             scrollX = TRUE
+                             # autoWidth = TRUE
+              ), escape = FALSE)
+  })
+}
+
+
 
 
