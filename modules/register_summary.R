9ba6c1707384b42cdbfc7636ed58875f42c67dc0
benjaminhlina <benjamin.hlina@gmail.com>
2026-01-06 11:46:22 -0500

    Commentred out regiser summary ---- would love to move this here and then call it in sidebar will get there

--- modules/register_summary.R
+++ modules/register_summary.R
@@ -1,23 +1,23 @@
-register_summary <- function(summary_info) {
-
-  observe({
-    df <- summary_info$summary_data()  # reactive from summary
-    output$download_summary <- downloadHandler(
-      filename = function() {
-        # get table name
-        tbl <- get_selected_table(main_input)
-        # make file name
-        paste0(tbl, "_summary_", Sys.Date(), ".xlsx")
-      },
-      # white out xslx
-      content = function(file) {
-        req(df)
-        writexl::write_xlsx(df, file)
-      }
-    )
-
-    # toggle button
-    shinyjs::toggleState(session$ns("download_summary"),
-                         condition = !is.null(df) && nrow(df) > 0)
-  })
-}
+# register_summary <- function(summary_info) {
+#
+#   observe({
+#     df <- summary_info$summary_data()  # reactive from summary
+#     output$download_summary <- downloadHandler(
+#       filename = function() {
+#         # get table name
+#         tables <- get_selected_tab(main_input)
+#         # make file name
+#         paste0(tbl, "_summary_", Sys.Date(), ".xlsx")
+#       },
+#       # white out xslx
+#       content = function(file) {
+#         req(df)
+#         writexl::write_xlsx(df, file)
+#       }
+#     )
+#
+#     # toggle button
+#     shinyjs::toggleState(session$ns("download_summary"),
+#                          condition = !is.null(df) && nrow(df) > 0)
+#   })
+# }
